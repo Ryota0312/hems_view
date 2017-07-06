6afbe1b25ef7263dddceb4a2ec447ba1071e0e4f
Ryota Nishi <puq77f8e@s.okayama-u.ac.jp>
2017-07-06 16:41:14 +0900

    Update script

---
+++ script/get_hemsdata.sample.rb
@@ -0,0 +1,34 @@
+require 'date'
+require 'net/https'
+require 'uri'
+require 'open-uri'
+require 'json'
+
+base_url = 'https://api.frameworxopendata.jp/api/v3/files/hems/'
+APIkey = '[YOUR_API_KEY]
+start_date = DateTime.new(2016, 4, 4)
+end_date = DateTime.new(2016, 4, 7)
+for date in start_date..end_date
+  url = "#{base_url}#{date.strftime("%Y%m%d")}.json?acl:consumerKey=#{APIkey}"
+  
+  res = JSON.parse(open(url).read)
+  puts "Get json(#{date})"
+  
+  for data in res
+    id = data["frameworx:hemsId"]
+    house = House.where("hems_id is ?", id).first
+    if house == nil
+      house = House.create(hems_id: id, postal: data["frameworx:postal"])
+    end
+    house.mains.create(value: data["frameworx:main"], timestamp: data["frameworx:date"])
+    house.solar_generates.create(value: data["frameworx:solarGenerated"], timestamp: data["frameworx:date"])
+    house.solar_sells.create(value: data["frameworx:solarSold"], timestamp: data["frameworx:date"])
+    house.battery_charges.create(value: data["frameworx:batteryCharge"], timestamp: data["frameworx:date"])
+    house.battery_discharges.create(value: data["frameworx:batteryDischarge"], timestamp: data["frameworx:date"])
+    house.fuel_cells.create(value: data["frameworx:fuelCell"], timestamp: data["frameworx:date"])
+    house.gus.create(value: data["frameworx:gusUsage"], timestamp: data["frameworx:date"])
+    house.waters.create(value: data["frameworx:waterUsage"], timestamp: data["frameworx:date"])
+    puts "#{id}(#{data[frameworx:date]}) is ok."
+  end
+  puts "#{date} is completed."
+end
--- script/test.rb
+++
@@ -1,8 +0,0 @@
-require 'date'
-
-s = DateTime.new(2016, 4, 1)
-e = DateTime.new(2017, 3, 31)
-
-for date in s..e
-  puts date
-end
